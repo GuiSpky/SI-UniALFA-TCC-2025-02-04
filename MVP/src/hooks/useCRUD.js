2a98e6378a9b86991fdc853d44d5f3adf4d874eb
Leonardo Chaves <127910384+chaves888@users.noreply.github.com>
2025-09-22 13:48:28 -0300

    Adicionando a tela do frontend
    CRUD de Usuários
    CRUD de Escolas
    CRUD de Cidades
    CRUD de Bairros
    CRUD de Cardápios
    
    Implementei também um Dark Mode (shadcn)

---
+++ MVP/src/hooks/useCRUD.js
@@ -0,0 +1,102 @@
+import { useState, useEffect, useRef } from 'react';
+import toast from 'react-hot-toast';
+import {getData, postData, updateData, deleteData} from '../api';
+
+
+const useCRUD = (entityType) => {
+  const didRun = useRef(false);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  
+  useEffect(() => {
+    if (didRun.current) return;
+    didRun.current = true;
+    getAll(entityType)
+  }, [entityType]);
+
+  const getAll = async (entityType) => {
+    setLoading(true);
+    try {
+      var dados = await getData(entityType);
+      setData(dados.data || []);
+      return dados;
+    } catch (err) {
+      setError(err.message);
+      toast.error('Erro ao carregar dados!');
+      throw err;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Criar novo item
+  const create = async (newItem) => {
+    setLoading(true);
+    try {
+      const item = {
+        ...newItem,
+        createdAt: new Date().toISOString().split('T')[0]
+      };
+      await postData(entityType, item);
+      await getAll(entityType);
+      toast.success('Item criado com sucesso!');
+      return item;
+    } catch (err) {
+      setError(err.message);
+      toast.error('Erro ao criar item');
+      throw err;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Atualizar item existente
+  const update = async (id, updatedItem) => {
+    setLoading(true);
+    try {
+      await updateData(entityType, id, updatedItem);
+      await getAll(entityType);
+      toast.success('Item atualizado com sucesso!');
+    } catch (err) {
+      setError(err.message);
+      toast.error('Erro ao atualizar item');
+      throw err;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const remove = async (id) => {
+    setLoading(true);
+    try {
+      await deleteData(entityType, id);
+      await getAll(entityType);
+      toast.success('Item removido com sucesso!');
+    } catch (err) {
+      setError(err.message);
+      toast.error('Erro ao remover item');
+      throw err;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const getById = (id) => {
+    return data.find(item => item.id === id);
+  };
+
+  return {
+    data,
+    loading,
+    error,
+    create,
+    update,
+    remove,
+    getById,
+    setData,
+    getAll
+  };
+};
+
+export default useCRUD;
