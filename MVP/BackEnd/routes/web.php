b4f1c5f6b0fed2bb5bfc1dc047f0d6bca10c1977
Guilherme Fernandes de Souza <guii0410@gmail.com>
2025-10-26 22:02:19 -0300

    login

--- MVP/BackEnd/routes/web.php
+++ MVP/BackEnd/routes/web.php
@@ -9,11 +9,8 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
+Route::get('/', function () {
     return view('dashboard');
 })->middleware('auth')->name('dashboard');
 
